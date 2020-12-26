0b01e5f081759bdb3d0072a63577434c48cd2b04
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 02:06:59 -0600

    Added Client Project

---
+++ src/Glader.ASP.ServiceDiscovery.Client/Client/IServiceDiscoveryService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using Refit;
+
+namespace Glader.ASP.ServiceDiscovery
+{
+	/// <summary>
+	/// Contract for REST service that provides
+	/// services discovery endpoints.
+	/// </summary>
+	[Headers("User-Agent: Glader")]
+	public interface IServiceDiscoveryService
+	{
+		/// <summary>
+		/// Attempts to discover a service with the provided <see cref="request"/>
+		/// model.
+		/// </summary>
+		/// <param name="request">The request.</param>
+		/// <returns>The result of the resolve request.</returns>
+		[Post("/api/ServiceDiscovery/Discover")]
+		Task<ResponseModel<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>> DiscoverServiceAsync([Body(BodySerializationMethod.Serialized)] ResolvedServiceEndpointRequest request);
+	}
+}
