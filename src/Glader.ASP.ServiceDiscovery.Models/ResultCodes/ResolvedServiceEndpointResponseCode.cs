434ba15296169de8f0fe63d9b1adaa75fd44f60a
HelloKitty <andrew.blakely@ymail.com>
2020-12-26 02:06:28 -0600

    Added ResolvedServiceEndpointResponseCode

---
+++ src/Glader.ASP.ServiceDiscovery.Models/ResultCodes/ResolvedServiceEndpointResponseCode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.ServiceDiscovery
+{
+	/// <summary>
+	/// Enumeration of all service endpoint response results.
+	/// </summary>
+	public enum ResolvedServiceEndpointResponseCode
+	{
+		/// <summary>
+		/// Indicates that the request was successful.
+		/// </summary>
+		Success = 1,
+
+		/// <summary>
+		/// Indicates that the service is unknown or unlisted by the directory.
+		/// </summary>
+		ServiceUnlisted = 2,
+
+		/// <summary>
+		/// Indicates that the service is not available right now.
+		/// </summary>
+		ServiceUnavailable = 3,
+
+		/// <summary>
+		/// Indicates that a general error has occured with the request.
+		/// </summary>
+		GeneralRequestError = 4
+	}
+}
