9d5a71a07cdc0eda8537a77a16e25ed91149e3fa
HelloKitty <andrew.blakely@ymail.com>
2020-12-30 23:24:44 -0600

    Added ServiceResolutionResponse

---
+++ src/Glader.ASP.ServiceDiscovery.Models/Models/ServiceResolutionResponse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Glader.ASP.ServiceDiscovery
+{
+	/// <summary>
+	/// Represents a response model to a full service resolution request.
+	/// </summary>
+	[JsonObject]
+	public sealed class ServiceResolutionResponse
+	{
+		/// <summary>
+		/// Internal-serialized <see cref="Services"/>
+		/// </summary>
+		[JsonProperty(PropertyName = "Services")]
+		internal ResolvedService[] _Services { get; set; } = Array.Empty<ResolvedService>();
+
+		/// <summary>
+		/// Represents the services as part of the service resolution request.
+		/// Can be empty.
+		/// </summary>
+		[JsonIgnore]
+		public IEnumerable<ResolvedService> Services => _Services;
+
+		/// <summary>
+		/// Creates a resolution response with the specified <see cref="Services"/>
+		/// </summary>
+		/// <param name="services">The resolved services.</param>
+		public ServiceResolutionResponse(ResolvedService[] services)
+		{
+			_Services = services ?? throw new ArgumentNullException(nameof(services));
+		}
+
+		/// <summary>
+		/// Creates an empty resolution response.
+		/// <see cref="Services"/> will be non-null but empty.
+		/// </summary>
+		[JsonConstructor]
+		public ServiceResolutionResponse()
+		{
+			
+		}
+	}
+}
