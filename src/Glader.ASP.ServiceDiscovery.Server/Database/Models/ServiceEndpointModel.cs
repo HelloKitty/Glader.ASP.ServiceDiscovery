fd7a5dc4a479b8b7ad214bc44a9f1cb8ebe3126b
HelloKitty <andrew.blakely@ymail.com>
2020-12-30 23:52:07 -0600

    Added name to ServiceEndpointModel

--- src/Glader.ASP.ServiceDiscovery.Server/Database/Models/ServiceEndpointModel.cs
+++ src/Glader.ASP.ServiceDiscovery.Server/Database/Models/ServiceEndpointModel.cs
@@ -10,11 +10,19 @@ namespace Glader.ASP.ServiceDiscovery
 	[Table("service_endpoints")]
 	public class ServiceEndpointModel
 	{
+		//Key is now based on composite between ServiceId and Name.
 		/// <summary>
 		/// Identifier of the service endpoint entry.
 		/// </summary>
-		[Key]
+		[Required]
 		public int ServiceId { get; internal set; }
+
+		/// <summary>
+		/// Represents the name of the endpoint.
+		/// (Different from service type).
+		/// </summary>
+		[Required]
+		public string Name { get; internal set; }
 
 		/// <summary>
 		/// The foreign relation service reference.
@@ -26,14 +34,16 @@ namespace Glader.ASP.ServiceDiscovery
 		/// <summary>
 		/// The endpoint address for the service.
 		/// </summary>
+		[Required]
 		public ResolvedEndpoint Endpoint { get; internal set; }
 
-		public ServiceEndpointModel(int serviceId, ResolvedEndpoint endpoint)
+		public ServiceEndpointModel(int serviceId, string name, ResolvedEndpoint endpoint)
 		{
 			if(serviceId <= 0) throw new ArgumentOutOfRangeException(nameof(serviceId));
 
 			ServiceId = serviceId;
 			Endpoint = endpoint ?? throw new ArgumentNullException(nameof(endpoint));
+			Name = name ?? throw new ArgumentNullException(nameof(name));
 		}
 
 		/// <summary>
