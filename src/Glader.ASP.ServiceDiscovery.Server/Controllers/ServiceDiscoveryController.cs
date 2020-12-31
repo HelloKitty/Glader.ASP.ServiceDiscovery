90f945661367c3e8575ae589055d598b1c855fb0
HelloKitty <andrew.blakely@ymail.com>
2020-12-31 00:03:17 -0600

    Implemented new Service interface on ASP Core controller

--- src/Glader.ASP.ServiceDiscovery.Server/Controllers/ServiceDiscoveryController.cs
+++ src/Glader.ASP.ServiceDiscovery.Server/Controllers/ServiceDiscoveryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Glader.ASP.ServiceDiscovery;
 using Glader.Essentials;
@@ -15,7 +16,7 @@ namespace Glader.ASP.ServiceDiscovery
 	/// Endpoints/actions are documented in the documentation repo.
 	/// </summary>
 	[Route("api/[controller]")]
-	public sealed class ServiceDiscoveryController : Controller
+	public sealed class ServiceDiscoveryController : BaseGladerController, IServiceDiscoveryService
 	{
 		/// <summary>
 		/// The endpoint repository.
@@ -25,44 +26,79 @@ namespace Glader.ASP.ServiceDiscovery
 		public ILogger<ServiceDiscoveryController> LoggingService { get; }
 
 		public ServiceDiscoveryController([FromServices] IServiceEndpointRepository endpointRepository, ILogger<ServiceDiscoveryController> loggingService)
+			: base(loggingService)
 		{
-			if (endpointRepository == null) throw new ArgumentNullException(nameof(endpointRepository));
-			if (loggingService == null) throw new ArgumentNullException(nameof(loggingService));
-
-			EndpointRepository = endpointRepository;
-			LoggingService = loggingService;
+			EndpointRepository = endpointRepository ?? throw new ArgumentNullException(nameof(endpointRepository));
+			LoggingService = loggingService ?? throw new ArgumentNullException(nameof(loggingService));
 		}
 
-		[HttpGet("{name}/" + nameof(Discover))]
-		public async Task<JsonResult> Discover([FromRoute(Name = "name")] string serviceName)
+		[ProducesJson]
+		[HttpGet("{name}/Single")]
+		public async Task<ResponseModel<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>> DiscoverServiceAsync(string serviceType, CancellationToken token = default)
 		{
 			if(LoggingService.IsEnabled(LogLevel.Debug))
-				LoggingService.LogDebug($"Service Discover request for: {serviceName}");
+				LoggingService.LogDebug($"Service Discover request for: {serviceType}");
 
 			if(!ModelState.IsValid)
 			{
 				if(LoggingService.IsEnabled(LogLevel.Debug))
 					LoggingService.LogDebug($"Resolution request was sent with an invalid model ModelState.");
 
-				Json(new ResponseModel<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>(ResolvedServiceEndpointResponseCode.GeneralRequestError));
+				return Failure<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>(ResolvedServiceEndpointResponseCode.GeneralRequestError);
 			}
 
 			//We need to check if we know about the locale
 			//If we don't we should indicate it is unlisted
 			//We also need to check if the keypair region and servicetype exist
 			//TODO: Deployment mode handling, right now it's set to INTERNAL
-			if(!await EndpointRepository.ContainsAsync(serviceName))
+			if(!await EndpointRepository.ContainsAsync(serviceType, token))
 			{
 				if(LoggingService.IsEnabled(LogLevel.Debug))
-					LoggingService.LogDebug($"Client requested unlisted service Service: {serviceName}.");
+					LoggingService.LogDebug($"Client requested unlisted service Service: {serviceType}.");
 
-				return Json(new ResponseModel<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>(ResolvedServiceEndpointResponseCode.ServiceUnlisted));
+				return Failure<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>(ResolvedServiceEndpointResponseCode.ServiceUnlisted);
 			}
 
 			//Assume no failure
-			ServiceEndpointModel endpoint = await EndpointRepository.RetrieveAsync(serviceName);
+			ServiceEndpointModel endpoint = await EndpointRepository.RetrieveAsync(serviceType, token);
 
-			return Json(new ResponseModel<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>(endpoint.Endpoint));
+			return Success<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>(endpoint.Endpoint);
+		}
+
+		[ProducesJson]
+		[HttpGet("{name}")]
+		public async Task<ResponseModel<ServiceResolutionResult, ResolvedServiceEndpointResponseCode>> DiscoverServicesAsync(string serviceType, CancellationToken token = default)
+		{
+			if(LoggingService.IsEnabled(LogLevel.Debug))
+				LoggingService.LogDebug($"Service Discover request for: {serviceType}");
+
+			if(!ModelState.IsValid)
+			{
+				if(LoggingService.IsEnabled(LogLevel.Debug))
+					LoggingService.LogDebug($"Resolution request was sent with an invalid model ModelState.");
+
+				return Failure<ServiceResolutionResult, ResolvedServiceEndpointResponseCode>(ResolvedServiceEndpointResponseCode.GeneralRequestError);
+			}
+
+			//We need to check if we know about the locale
+			//If we don't we should indicate it is unlisted
+			//We also need to check if the keypair region and servicetype exist
+			//TODO: Deployment mode handling, right now it's set to INTERNAL
+			if(!await EndpointRepository.ContainsAsync(serviceType, token))
+			{
+				if(LoggingService.IsEnabled(LogLevel.Debug))
+					LoggingService.LogDebug($"Client requested unlisted service Service: {serviceType}.");
+
+				return Failure<ServiceResolutionResult, ResolvedServiceEndpointResponseCode>(ResolvedServiceEndpointResponseCode.ServiceUnlisted);
+			}
+
+			//Assume no failure
+			ResolvedService[] endpoints = (await EndpointRepository
+					.RetrieveAllAsync(serviceType, token))
+				.Select(m => new ResolvedService(m.Name, m.Endpoint))
+				.ToArray();
+
+			return Success<ServiceResolutionResult, ResolvedServiceEndpointResponseCode>(new ServiceResolutionResult(endpoints));
 		}
 	}
 }
