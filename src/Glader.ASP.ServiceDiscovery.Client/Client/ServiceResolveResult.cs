b4112904b455dcc374c0ab1cf6a926e464f549b4
HelloKitty <andrew.blakely@ymail.com>
2021-04-07 00:08:09 -0500

    Added ServiceResolveResult

---
+++ src/Glader.ASP.ServiceDiscovery.Client/Client/ServiceResolveResult.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.ServiceDiscovery
+{
+	/// <summary>
+	/// Model for representing the result of a service resolve attempt.
+	/// </summary>
+	/// <typeparam name="TServiceType">The service type.</typeparam>
+	public sealed class ServiceResolveResult<TServiceType> : ResponseModel<TServiceType, ResolvedServiceEndpointResponseCode>
+		where TServiceType : class
+	{
+		/// <summary>
+		/// Indicates if the service is available.
+		/// </summary>
+		public bool isAvailable => base.isSuccessful;
+
+		/// <summary>
+		/// Resolved instance.
+		/// </summary>
+		public TServiceType Instance => base.Result;
+
+		/// <summary>
+		/// Creates a successful resolve attempt.
+		/// Instance of the service must be non-null.
+		/// </summary>
+		/// <param name="instance">The service instance.</param>
+		public ServiceResolveResult(TServiceType instance)
+			: base(instance)
+		{
+
+		}
+
+		/// <summary>
+		/// Creates a failed resolve result.
+		/// </summary>
+		public ServiceResolveResult()
+			: base(ResolvedServiceEndpointResponseCode.GeneralRequestError)
+		{
+
+		}
+
+		/// <summary>
+		/// Creates a failed resolve result.
+		/// </summary>
+		public ServiceResolveResult(ResolvedServiceEndpointResponseCode failureCode)
+			: base(failureCode)
+		{
+			if (failureCode == ResolvedServiceEndpointResponseCode.Success)
+				throw new ArgumentException($"Cannot use Success code for failed response mode.", nameof(failureCode));
+		}
+	}
+}
