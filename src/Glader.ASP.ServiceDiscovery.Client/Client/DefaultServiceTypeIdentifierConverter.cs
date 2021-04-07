1d7eb6337d73976d356d066da9d1e275c559877b
HelloKitty <andrew.blakely@ymail.com>
2021-04-07 00:08:55 -0500

    Added DefaultServiceTypeIdentifierConverter

---
+++ src/Glader.ASP.ServiceDiscovery.Client/Client/DefaultServiceTypeIdentifierConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.ServiceDiscovery
+{
+	/// <summary>
+	/// Default <see cref="IServiceTypeIdentifierConverter{TServiceTypeEnum}"/> implementation converts the enum to a string.
+	/// </summary>
+	/// <typeparam name="TServiceTypeEnum"></typeparam>
+	public sealed class DefaultServiceTypeIdentifierConverter<TServiceTypeEnum> : IServiceTypeIdentifierConverter<TServiceTypeEnum> 
+		where TServiceTypeEnum : Enum
+	{
+		public string Create(TServiceTypeEnum context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+			return context.ToString();
+		}
+	}
+}
