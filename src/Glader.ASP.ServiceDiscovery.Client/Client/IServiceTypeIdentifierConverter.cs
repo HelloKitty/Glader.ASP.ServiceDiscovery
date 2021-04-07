fd3a122cd5f9b724c1c784eb2ed17a7bae86a37b
HelloKitty <andrew.blakely@ymail.com>
2021-04-07 00:08:25 -0500

    Added IServiceTypeIdentifier

---
+++ src/Glader.ASP.ServiceDiscovery.Client/Client/IServiceTypeIdentifierConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace Glader.ASP.ServiceDiscovery
+{
+	/// <summary>
+	/// Contract for types that can convert the service type enum value to an identifier name.
+	/// </summary>
+	/// <typeparam name="TServiceTypeEnum">The service enum type.</typeparam>
+	public interface IServiceTypeIdentifierConverter<in TServiceTypeEnum> : IFactoryCreatable<string, TServiceTypeEnum>
+		where TServiceTypeEnum : Enum
+	{
+
+	}
+}
