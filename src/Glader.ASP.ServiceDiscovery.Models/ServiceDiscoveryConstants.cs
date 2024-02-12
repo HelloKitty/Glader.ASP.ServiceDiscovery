d8e7127cc565f08c254fe30d0a7dff99fa0d4841
HelloKitty <andrew.blakely@ymail.com>
2024-02-12 02:30:17 -0600

    Create ServiceDiscoveryConstants.cs

---
+++ src/Glader.ASP.ServiceDiscovery.Models/ServiceDiscoveryConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glader.ASP.ServiceDiscovery
+{
+	public static class ServiceDiscoveryConstants
+	{
+		/// <summary>
+		/// The default group name.
+		/// </summary>
+		public const string DEFAULT_GROUP_NAME = "Default";
+	}
+}
