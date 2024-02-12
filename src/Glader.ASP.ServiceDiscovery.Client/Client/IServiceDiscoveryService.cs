b72bc1243e6ea5bc63eca140940a7367c7456405
HelloKitty <andrew.blakely@ymail.com>
2024-02-12 15:45:06 -0600

    Added DiscoverServiceAsync with groupname support

--- src/Glader.ASP.ServiceDiscovery.Client/Client/IServiceDiscoveryService.cs
+++ src/Glader.ASP.ServiceDiscovery.Client/Client/IServiceDiscoveryService.cs
@@ -23,6 +23,16 @@ namespace Glader.ASP.ServiceDiscovery
 		/// <param name="token"></param>
 		/// <returns>The result of the resolve request.</returns>
 		[Get("/api/ServiceDiscovery/{name}/Single")]
+		Task<ResponseModel<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>> DiscoverServiceAsync([AliasAs("name")] string serviceType, string groupName, CancellationToken token = default);
+
+		/// <summary>
+		/// Attempts to discover a service with the provided <see cref="serviceType"/>
+		/// model.
+		/// </summary>
+		/// <param name="serviceType">The type of the service.</param>
+		/// <param name="token"></param>
+		/// <returns>The result of the resolve request.</returns>
+		[Get("/api/ServiceDiscovery/{name}/Single")]
 		Task<ResponseModel<ResolvedEndpoint, ResolvedServiceEndpointResponseCode>> DiscoverServiceAsync([AliasAs("name")] string serviceType, CancellationToken token = default);
 
 		/// <summary>
